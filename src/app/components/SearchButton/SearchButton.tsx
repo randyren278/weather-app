2878baf3d62691c1059a43463015193ab414a1c5
randyren278 <randyren278@gmail.com>
2024-07-21 15:00:48 -0600

    fixed search button attributes

--- src/app/components/SearchButton/SearchButton.tsx
+++ src/app/components/SearchButton/SearchButton.tsx
@@ -1,13 +1,29 @@
-"use client"
+"use client";
 
-import React from 'react'
+import { Dialog,DialogTrigger } from "@radix-ui/react-dialog";
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { commandIcon } from "@/app/utils/icons";
 
 function SearchButton() {
   return (
     <div className="search-btn">
-    SearchButton  
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button
+            variant="outline"
+            className="border inline-flex items-center justify-center text-sm font-medium hover:dark:bg-[#131313] hover:bg-slate-100  ease-in-out duration-200"
+          >
+            <p className="text-sm text-muted-foreground">Search Here...</p>
+            <div className="command dark:bg-[#262626] bg-slate-200  py-[2px] pl-[5px] pr-[7px] rounded-sm ml-[10rem] flex items-center gap-2">
+              {commandIcon}
+              <span className="text-[9px]">F</span>
+            </div>
+          </Button>
+        </DialogTrigger>
+      </Dialog>
     </div>
-  )
+  );
 }
 
-export default SearchButton
+export default SearchButton;
