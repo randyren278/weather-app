b336b60dbb21aec53911fc4d119eb7bb21783702
randyren278 <randyren278@gmail.com>
2024-07-22 01:48:27 -0600

    added live time and date

--- src/app/components/Temperature/Temperature.tsx
+++ src/app/components/Temperature/Temperature.tsx
@@ -1,6 +1,17 @@
 "use client";
 import { useGlobalContext } from "@/app/context/globalContext";
+import { kelvintoCelsius } from "@/app/utils/misc";
 import React from "react";
+import { useState,useEffect } from "react";
+import {
+    clearSky,
+    cloudy,
+    drizzleIcon,
+    navigation,
+    rain,
+    snow,
+  } from "@/app/utils/icons";
+import moment from "moment"
 
 function Temperature() {
   const { forecast } = useGlobalContext();
@@ -8,12 +19,77 @@ function Temperature() {
   const { main, timezone, name, weather } = forecast;
 
   if (!forecast || !weather) return <div>Loading...</div>;
+
+  const temp = kelvintoCelsius(main?.temp);
+  const minTemp = kelvintoCelsius(main?.temp_min);
+  const maxTemp = kelvintoCelsius(main?.temp_max);
+
+  //state
+  const [localTime, setLocalTime] = useState<string>("");
+  const [currentDay, setCurrentDay] = useState<string>("");
+
+  const { main: weatherMain, description } = weather[0];
+
+
+  const getIcon = () => {
+    switch (weatherMain) {
+      case "Drizzle":
+        return drizzleIcon;
+      case "Rain":
+        return rain;
+      case "Snow":
+        return snow;
+      case "Clear":
+        return clearSky;
+      case "Clouds":
+        return cloudy;
+      default:
+        return clearSky;
+    }
+  };
+
+  useEffect(() => {
+    // upadte time every second
+    const interval = setInterval(() => {
+      const localMoment = moment().utcOffset(timezone / 60);
+      // custom format: 24 hour format
+      const formatedTime = localMoment.format("HH:mm:ss");
+      // day of the week
+      const day = localMoment.format("dddd");
+
+      setLocalTime(formatedTime);
+      setCurrentDay(day);
+    }, 1000);
+
+    // clear interval
+    return () => clearInterval(interval);
+  }, [timezone]);
+
   return (
     <div
-      className="pt-6 pb-5 border rounded-lg flex-col 
+      className="pt-6 pb-5 px-4 border rounded-lg flex flex-col 
         justify-between dark:bg-dark-grey shadow-sm dark:shadow-none"
     >
-      Temperature
+      <p className="flex justify-between items-center">
+        <span className="font-medium">{currentDay}</span>
+        <span className="font-medium">{localTime}</span>
+      </p>
+      <p className="pt-2 font-bold flex gap-1">
+        <span>{name}</span>
+        <span>{navigation}</span>
+      </p>
+      <p className="py-10 text-9xl font-bold self-center">{temp}°</p>
+
+      <div>
+        <div>
+          <span>{getIcon()}</span>
+          <p className="pt-2 capitalize text-lg font-medium">{description}</p>
+        </div>
+        <p className="flex items-center gap-2">
+          <span>Low: {minTemp}°</span>
+          <span>High: {maxTemp}°</span>
+        </p>
+      </div>
     </div>
   );
 }
---
+++ src/app/utils/misc.tsx
@@ -0,0 +1,3 @@
+export const kelvintoCelsius = (kelvin: number) => {
+  return Math.round(kelvin - 273.15);
+};
