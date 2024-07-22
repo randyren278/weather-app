0b3e3b1f140296737027477cb215661a7124bb37
randyren278 <randyren278@gmail.com>
2024-07-21 23:02:42 -0600

    fixed api key

--- src/app/api/weather/route.ts
+++ src/app/api/weather/route.ts
@@ -3,15 +3,15 @@ import axios from "axios";
 
 export async function GET(req: NextRequest) {
   try {
-    const apiKey = process.env.OEPNWEATHERMAP_API_KEY;
+    const apiKey = process.env.OPENWEATHERMAP_API_KEY;
     const lat = 40.4165;
     const lon = -3.7026;
-    const url = "https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}";
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
     const res = await axios.get(url);
     return NextResponse.json(res.data);
   } catch (error) {
-    console.log("Error fetching forecast data");
+    console.log("Error fetching forecast data:");
 
     return new Response("Error fetching forecast data", { status: 500 });
   }
--- src/app/context/globalContext.js
+++ src/app/context/globalContext.js
@@ -2,6 +2,7 @@
 
 import axios from "axios";
 import React, { createContext, useContext, useEffect, useState } from "react";
+import defaultStates from "../utils/defaultstates";
 
 const GlobalContext = createContext();
 const GlobalContextUpdate = createContext();
@@ -9,9 +10,9 @@ const GlobalContextUpdate = createContext();
 export const GlobalContextProvider = ({ children }) => {
   const [forecast, setForecast] = useState({});
 
-  const fetchForecast = async () => {
+  const fetchForecast = async (lat, lon) => {
     try {
-      const res = await axios.get("api/weather");
+      const res = await axios.get(`/api/weather?lat=${lat}&lon=${lon}`); // Corrected template literal
       setForecast(res.data);
     } catch (error) {
       console.log("Error fetching forecast data:", error.message);
@@ -19,13 +20,14 @@ export const GlobalContextProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    fetchForecast();
+    const defaultLat = 40.4165; // Default latitude
+    const defaultLon = -3.7026; // Default longitude
+    fetchForecast(defaultLat, defaultLon);
   }, []);
+
   return (
-    <GlobalContext.Provider value={{
-        forecast
-    }}>
-      <GlobalContextUpdate.Provider>{children}</GlobalContextUpdate.Provider>
+    <GlobalContext.Provider value={{ forecast }}>
+      <GlobalContextUpdate.Provider value={setForecast}>{children}</GlobalContextUpdate.Provider>
     </GlobalContext.Provider>
   );
 };
