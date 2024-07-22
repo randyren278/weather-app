e74c07209e48620e547008abf7cfeb6f49d562be
randyren278 <randyren278@gmail.com>
2024-07-21 19:48:01 -0600

    added api keys (hidden)

--- src/app/api/weather/route.ts
+++ src/app/api/weather/route.ts
@@ -2,6 +2,11 @@ import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
   try {
+    const apiKey=process.env.OPENWEATHERMAP_API_KEY;
+    const lat = 40.4165;
+    const lon = -3.7026;
+    const url =
+      "https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API key}";
   } catch (error) {
     console.log("Error fetching forecast data");
 
