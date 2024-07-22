743846c7fe0e2bef6da55f49ff356b069e6ca235
randyren278 <randyren278@gmail.com>
2024-07-21 20:49:26 -0600

    started weather div

--- src/app/context/globalContext.js
+++ src/app/context/globalContext.js
@@ -1,16 +1,31 @@
 "use client";
 
-import React, { createContext, useContext, useState } from "react";
+import axios from "axios";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 const GlobalContext = createContext();
 const GlobalContextUpdate = createContext();
 
 export const GlobalContextProvider = ({ children }) => {
+  const [forecast, setForecast] = useState({});
+
+  const fetchForecast = async () => {
+    try {
+      const res = await axios.get("api/weather");
+      setForecast(res.data);
+    } catch (error) {
+      console.log("Error fetching forecast data:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchForecast();
+  }, []);
   return (
-    <GlobalContext.Provider value="hello">
-      <GlobalContextUpdate.Provider>
-        {children}
-      </GlobalContextUpdate.Provider>
+    <GlobalContext.Provider value={{
+        forecast
+    }}>
+      <GlobalContextUpdate.Provider>{children}</GlobalContextUpdate.Provider>
     </GlobalContext.Provider>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <main className="mx-[1-rem] lg:mx-[2-rem] x1:mx-[6rem] 2xl:mx-[16rem] m-auto">
       <Navbar />
+      <div className="pb-4 flex flex-col gap-4 md:flex-row">
+        <div className="flex flex-col gao-4 w-full min-w-[18rem] md:w-[35rem]"></div>
+        <div className="flex flex-col"></div>
+      </div>
     </main>
   );
 }
