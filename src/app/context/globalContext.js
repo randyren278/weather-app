0c3aac8348825eeef68292dac28b342e07f9d4ea
randyren278 <randyren278@gmail.com>
2024-07-23 15:10:53 -0600

    fixed naming scheme search functionality is working now

--- src/app/context/globalContext.js
+++ src/app/context/globalContext.js
@@ -1,29 +1,29 @@
 "use client";
-
 import axios from "axios";
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { useContext, createContext, useState, useEffect } from "react";
 import defaultStates from "../utils/defaultstates";
+import { debounce } from "lodash";
 
 const GlobalContext = createContext();
 const GlobalContextUpdate = createContext();
 
 export const GlobalContextProvider = ({ children }) => {
   const [forecast, setForecast] = useState({});
-  const [airQuality, setAirQuality] = useState({});
-  const [fiveDayForecast, setFiveDayForecast] = useState({});
-  const [uvIndex, setUvIndex] = useState({});
   const [geoCodedList, setGeoCodedList] = useState(defaultStates);
   const [inputValue, setInputValue] = useState("");
   const [activeCityCoords, setActiveCityCoords] = useState([
     51.752021, -1.257726,
   ]);
+  const [airQuality, setAirQuality] = useState({});
+  const [fiveDayForecast, setFiveDayForecast] = useState({});
+  const [uvIndex, setUvIndex] = useState({});
 
   const fetchForecast = async (lat, lon) => {
     try {
       const res = await axios.get(`/api/weather?lat=${lat}&lon=${lon}`);
       setForecast(res.data);
     } catch (error) {
-      console.log("Error fetching forecast data:", error.message);
+      console.log("Error fetching forecast data: ", error.message);
     }
   };
 
@@ -32,7 +32,7 @@ export const GlobalContextProvider = ({ children }) => {
       const res = await axios.get(`/api/pollution?lat=${lat}&lon=${lon}`);
       setAirQuality(res.data);
     } catch (error) {
-      console.log("Error fetching air pollution data: ", error.message);
+      console.log("Error fetching air quality data: ", error.message);
     }
   };
 
@@ -45,15 +45,6 @@ export const GlobalContextProvider = ({ children }) => {
     }
   };
 
-  const fetchUvIndex = async (lat, lon) => {
-    try {
-      const res = await axios.get(`/api/uv?lat=${lat}&lon=${lon}`);
-      setUvIndex(res.data);
-    } catch (error) {
-      console.error("Error fetching the UV index:", error);
-    }
-  };
-
   const fetchGeoCodedList = async (search) => {
     try {
       const res = await axios.get(`/api/geocoded?search=${search}`);
@@ -63,29 +54,54 @@ export const GlobalContextProvider = ({ children }) => {
     }
   };
 
+  const fetchUvIndex = async (lat, lon) => {
+    try {
+      const res = await axios.get(`/api/uv?lat=${lat}&lon=${lon}`);
+      setUvIndex(res.data);
+    } catch (error) {
+      console.error("Error fetching the UV index:", error);
+    }
+  };
+
   const handleInput = (e) => {
     setInputValue(e.target.value);
-
     if (e.target.value === "") {
       setGeoCodedList(defaultStates);
     }
   };
 
   useEffect(() => {
+    const debouncedFetch = debounce((search) => {
+      fetchGeoCodedList(search);
+    }, 500);
+
+    if (inputValue) {
+      debouncedFetch(inputValue);
+    }
+    return () => debouncedFetch.cancel();
+  }, [inputValue]);
+
+  useEffect(() => {
     fetchForecast(activeCityCoords[0], activeCityCoords[1]);
     fetchAirQuality(activeCityCoords[0], activeCityCoords[1]);
     fetchFiveDayForecast(activeCityCoords[0], activeCityCoords[1]);
     fetchUvIndex(activeCityCoords[0], activeCityCoords[1]);
-    fetchGeoCodedList("liverpool");
   }, [activeCityCoords]);
 
   return (
     <GlobalContext.Provider
-      value={{ forecast, airQuality, fiveDayForecast, uvIndex, geoCodedList, inputValue, handleInput,setActiveCityCoords }}
+      value={{
+        forecast,
+        airQuality,
+        fiveDayForecast,
+        uvIndex,
+        geoCodedList,
+        inputValue,
+        handleInput,
+        setActiveCityCoords,
+      }}
     >
-      <GlobalContextUpdate.Provider
-        value={{ setActiveCityCoords}}
-      >
+      <GlobalContextUpdate.Provider value={{ setActiveCityCoords }}>
         {children}
       </GlobalContextUpdate.Provider>
     </GlobalContext.Provider>
