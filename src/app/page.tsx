972a70a5cd82aa8af752e151d8270a37f672f382
randyren278 <randyren278@gmail.com>
2024-07-24 00:48:27 -0600

    edited client again for deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,14 +27,18 @@ export default function Home() {
   }, []);
 
   const getClickedCityCords = (lat: number, lon: number) => {
-    setActiveCityCoords([lat, lon]);
     if (isClient) {
+      setActiveCityCoords([lat, lon]);
       window.scrollTo({
         top: 0,
         behavior: "smooth",
       });
     }
   };
+
+  if (!isClient) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
@@ -68,19 +72,15 @@ export default function Home() {
                   Top Large Cities
                 </h2>
                 <div className="flex flex-col gap-4">
-                  {defaultstates.map((state, index) => {
-                    return (
-                      <div
-                        key={index}
-                        className="border rounded-lg cursor-pointer dark:bg-dark-grey shadow-sm dark:shadow-none"
-                        onClick={() => {
-                          getClickedCityCords(state.lat, state.lon);
-                        }}
-                      >
-                        <p className="px-6 py-4">{state.name}</p>
-                      </div>
-                    );
-                  })}
+                  {defaultstates.map((state, index) => (
+                    <div
+                      key={index}
+                      className="border rounded-lg cursor-pointer dark:bg-dark-grey shadow-sm dark:shadow-none"
+                      onClick={() => getClickedCityCords(state.lat, state.lon)}
+                    >
+                      <p className="px-6 py-4">{state.name}</p>
+                    </div>
+                  ))}
                 </div>
               </div>
             </div>
