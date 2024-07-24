4ec9c14a4eb986c5585ef1b66ded0c531e876260
randyren278 <randyren278@gmail.com>
2024-07-24 01:32:59 -0600

    final reformat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,13 +58,13 @@ export default function Home() {
           <div className="flex flex-col w-full">
             <div className="instruments grid h-full gap-4 col-span-full sm-2:col-span-2 lg:grid-cols-3 xl:grid-cols-4">
               <DailyForecast />
-              <AirPollution />
+              <Visibility />
+              <Pressure />
               <UvIndex />
               <Population />
               <Sunset />
               <Compass />
-              <Visibility />
-              <Pressure />
+              <AirPollution />
               <FeelsLike />
               <Humidity />
             </div>
