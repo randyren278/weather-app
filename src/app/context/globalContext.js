006a656fb6447dd7eabfad006663efeecda37610
randyren278 <randyren278@gmail.com>
2024-07-23 19:15:57 -0600

    changed defeault city

--- src/app/context/globalContext.js
+++ src/app/context/globalContext.js
@@ -12,7 +12,7 @@ export const GlobalContextProvider = ({ children }) => {
   const [geoCodedList, setGeoCodedList] = useState(defaultStates);
   const [inputValue, setInputValue] = useState("");
   const [activeCityCoords, setActiveCityCoords] = useState([
-    51.752021, -1.257726,
+    40.730610, -73.935242,
   ]);
   const [airQuality, setAirQuality] = useState({});
   const [fiveDayForecast, setFiveDayForecast] = useState({});
